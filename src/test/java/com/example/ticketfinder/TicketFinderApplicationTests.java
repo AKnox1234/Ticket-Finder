fc9687f1ddd9e3e89e3660f97d2eb558dae5010c
AKnox1234 <90276429+AKnox1234@users.noreply.github.com>
2023-05-27 14:08:35 +0100

    Initial commit

---
+++ src/test/java/com/example/ticketfinder/TicketFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
