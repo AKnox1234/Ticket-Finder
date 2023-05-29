7cbb292eac5f33970c2f45069bb703d545a9a281
tothgbalint <tothg.balint@gmail.com>
2023-05-29 22:29:42 +0200

    minor editing

--- src/main/java/com/example/ticketfinder/Controller/TicketController.java
+++ src/main/java/com/example/ticketfinder/Controller/TicketController.java
@@ -1,4 +1,13 @@
 package com.example.ticketfinder.Controller;
 
+import com.example.ticketfinder.DAO.TicketDAO;
+import com.example.ticketfinder.DAO.TicketDaoDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TicketController {
+
+    @Autowired
+    TicketDaoDB ticketDaoDB;
 }
--- src/main/java/com/example/ticketfinder/Controller/UserController.java
+++ src/main/java/com/example/ticketfinder/Controller/UserController.java
@@ -1,4 +1,13 @@
 package com.example.ticketfinder.Controller;
 
+import com.example.ticketfinder.DAO.TicketDaoDB;
+import com.example.ticketfinder.DAO.UserDaoDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+
+    @Autowired
+    UserDaoDB userDaoDB;
 }
