f69ac850d6f6eea5d525aacd4383968ecc218f3a
tothgbalint <tothg.balint@gmail.com>
2023-05-29 18:33:49 +0200

    fixed issue where database didn't have entries for certain artists
    implemented User and Ticket classes, including getters + setters, equals, hashcode methods
    
    implemented CRUD methods for User and Ticket in UserDaoDB and TicketDaoDB
    
    UserDaoDB:
    findUserById
    addUser
    deleteUser
    updateUser
    getTicketsOfUser
    UserMapper class
    
    TicketDaoDB:
    findTicketByTicketId
    addTicket
    deleteTicket
    updateTicket
    TicketMapper class

--- src/main/java/com/example/ticketfinder/Entities/Ticket.java
+++ src/main/java/com/example/ticketfinder/Entities/Ticket.java
@@ -1,4 +1,47 @@
 package com.example.ticketfinder.Entities;
 
+import java.util.Objects;
+
 public class Ticket {
+
+    private int id;
+
+    private int concertId;
+    private float price;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getConcertId() {
+        return concertId;
+    }
+
+    public void setConcertId(int concertId) {
+        this.concertId = concertId;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return id == ticket.id && concertId == ticket.concertId && Float.compare(ticket.price, price) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, concertId, price);
+    }
 }
