ff5d0bd35ceda73389e61a9355b96f66e1259b21
tothgbalint <tothg.balint@gmail.com>
2023-05-30 12:51:35 +0200

    lots of frontend stuff
    somehow the new pages don't use their CSS files the way they did before I put the into the project
    
    both new pages are accessable through the app, and Create Account page is on Sign In page

--- src/main/java/com/example/ticketfinder/controller/UserController.java
+++ src/main/java/com/example/ticketfinder/controller/UserController.java
@@ -1,12 +1,45 @@
 package com.example.ticketfinder.controller;
 
 import com.example.ticketfinder.dao.UserDaoDB;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
     @Autowired
     UserDaoDB userDaoDB;
+
+    @GetMapping("createAccount")
+    public String createAccount() {
+        return "createAccount";
+    }
+
+    @GetMapping("signIn")
+    public String signIn() {
+        return "signIn";
+    }
+
+    /*@PostMapping("createAccount")
+    public String createAccount(HttpServletRequest request) {
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User user = new User();
+        teacher.setFirstName(firstName);
+        teacher.setLastName(lastName);
+        teacher.setSpecialty(specialty);
+
+        teacherDao.addTeacher(teacher);
+
+        return "redirect:/teachers";
+    }*/
+
 }
