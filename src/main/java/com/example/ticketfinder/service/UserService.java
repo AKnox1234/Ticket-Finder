be81348c7e511c44c2ab1b0d5197ade1989749b7
tothgbalint <tothg.balint@gmail.com>
2023-06-04 22:41:45 +0200

    inplemented user validity and admin account check

--- src/main/java/com/example/ticketfinder/service/UserService.java
+++ src/main/java/com/example/ticketfinder/service/UserService.java
@@ -1,4 +1,41 @@
 package com.example.ticketfinder.service;
 
+import com.example.ticketfinder.dao.UserDao;
+import com.example.ticketfinder.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    /**
+     *
+     * @param email
+     * @param password
+     * @return - returns TRUE if email an password corresponds to existing AND valid user, otherwise returns false
+     */
+    public boolean validUserCheck(String email, String password) {
+
+        try {
+
+            User user = userDao.findByEmail(email);
+            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+            return bCryptPasswordEncoder.matches(password, user.getPassword());
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean isAdminCheck(String email) {
+
+        User user = userDao.findByEmail(email);
+        if (user.getUserType().equalsIgnoreCase("admin")) {return true;}
+        return false;
+    }
 }
